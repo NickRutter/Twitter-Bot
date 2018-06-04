733e14c066b61a6b172b1466391cdfee58ab7550
NickRutter <36086506+NickRutter@users.noreply.github.com>
2018-06-04 09:58:24 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var twit = require('twit');
 
 module.exports = {
-  consumer_key: 'EckNr2p6RnO5J6jDVL8GVh6HA',
-  consumer_secret: 'gsCZuPWt2tRsujIYyArtnvsDfIWiunVhCnaEI7wcuzk39Rycyv',
-  access_token: '750014056543744000-lG1AZnVvBawAnCd747QYXI0ITEjO0oK',
-  access_token_secret: 'wkUSt6YrpMjRcu36ybWVBfHAuEKudDt2ckZ0ueOiVXXTP'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 }
